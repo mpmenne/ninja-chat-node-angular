34c5141a1433016ec10e91fdc7f07a855f0ee559
MoMenne <mpmenne@electronicsystemssupport.com>
2013-01-10 19:45:11 -0600

    all done

--- js/controllers.js
+++ js/controllers.js
@@ -4,21 +4,32 @@
 
 function ChatController($scope, socket) {
 
+
     $scope.chats = [
-        {'name':'bruce lee', 'message':'I kill you with one finger'},
-        {'name':'jackie chan', 'message': 'I kill you with my sense of humor'}
+        {'name':'jackie chan', 'message': 'I kill you with my sense of humor', color: 'orange'},
+        {'name':'bruce lee', 'message':'I kill you with one finger', color:'green'}
     ];
 
+    $scope.ninja = "";
+
+    $scope.start = function()  {
+        if (!$scope.ninja) {
+            $scope.ninja = prompt("Who are you???");
+            socket.emit("new ninja", $scope.ninja);
+        }
+    };
+
+    $scope.start();
+
     socket.on('update ninjas', function (data) {
-        $scope.chats.unshift({name:'anonymous', message:data.msg});
+        $scope.chats.unshift({name:data.name, message:data.msg});
         console.log(data);
     });
 
     $scope.addMessage = function(msg) {
-        var message = {name: 'me', msg: msg};
-        $scope.chats.unshift({name:'me', message: message.msg});
-        socket.emit("new message", message);
+        $scope.chats.unshift({name:'me', message: msg});
         $scope.msg = '';
+        socket.emit("new message", {name: $scope.ninja, msg: msg});
     };
 
 }
