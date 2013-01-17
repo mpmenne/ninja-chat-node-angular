56a3de7234e1379f701ac48318a046c9bf360afb
MoMenne <mpmenne@electronicsystemssupport.com>
2013-01-16 20:42:20 -0600

    removing some unnecessary

--- server.js
+++
@@ -1,7 +0,0 @@
-var http = require("http");
-
-http.createServer(function(request, response) {
-   response.writeHead(200, {"Content-Type":"text/plain"});
-   response.write("Hello World 1");
-   response.end();
-}).listen(8889);
